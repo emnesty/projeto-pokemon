a2636f25be588fa6f3d9b6f93deb330eae210b83
Luciano Silva <legostr@gmail.com>
2023-03-15 20:57:01 -0300

    Nova seção e header

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,9 +1,27 @@
+//Scripts do slide principal
+var slide_hero = new Swiper(".slide-hero", {
+  effect: 'fade',
+  pagination: {
+    el: "",
+  },
+});
+
+
+
 const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
+const bntCloseModal = document.querySelector('.js-close-modal-details-pokemon')
 
 function openDetailsPokemon() {
   document.documentElement.classList.add('open-modal');
 }
 
+function closeDetailsPokemon() {
+  document.documentElement.classList.remove('open-modal');
+}
+
 cardPokemon.forEach(card => {
   card.addEventListener('click', openDetailsPokemon);
 })
+
+bntCloseModal.addEventListener('click', closeDetailsPokemon);
+
